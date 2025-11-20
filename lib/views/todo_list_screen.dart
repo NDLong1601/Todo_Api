07315818d15c9f89929c1e695ffa924f18091810
NguyenLong <nguyenlong160104@gmail.com>
2025-11-20 19:18:02 +0700

    .

--- lib/views/todo_list_screen.dart
+++ lib/views/todo_list_screen.dart
@@ -1,11 +1,39 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:todo_api/const/app_color.dart';
+import 'package:todo_api/views/add_task.dart';
 
 class TodoListScreen extends StatelessWidget {
   const TodoListScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text('task')));
+    return Scaffold(
+      body: Stack(
+        children: [
+          // ListView.separated(itemBuilder: itemBuilder, separatorBuilder: separatorBuilder, itemCount: itemCount),
+          Positioned(
+            right: 22,
+            bottom: 89,
+            child: GestureDetector(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (_) => AddTask()),
+                );
+              },
+              child: Container(
+                height: 70,
+                width: 70,
+                decoration: BoxDecoration(
+                  color: AppColor.purple,
+                  shape: BoxShape.circle,
+                ),
+                child: Icon(Icons.add, color: Colors.white, size: 36),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
